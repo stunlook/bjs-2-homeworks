79a9460e19a98804c2c730b024da17cad9b796f5
Stepan Smirnov <stunlooksteff@gmail.com>
2023-05-30 20:40:57 +0300

    Задание выполнено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,31 @@
+"use strict";
+
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    return this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+    if ("marks" in this) {
+        this.marks.push(...marksToAdd);
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if ("marks" in this && this.marks.length !== 0) {
+        return this.marks.reduce((item, next) => item + next, 0) / this.marks.length;        
+    }
+    return 0;    
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    return this.excluded = reason;
 }
