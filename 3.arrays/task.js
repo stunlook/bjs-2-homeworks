eaf234278e22465e2dee7ed174267e950cede77b
Stepan Smirnov <stunlooksteff@gmail.com>
2023-05-30 20:31:09 +0300

    Выполнено два задания

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,24 @@
-function compareArrays(arr1, arr2) {
-  
-}
+"use strict";
 
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
+function compareArrays(arr1, arr2) {
+    if (arr1.length === arr2.length) {
+      return arr1.every((element, i) => element === arr2[i]); 
+    }
+    return false;
+  }
+
+  function getUsersNamesInAgeRange(users, gender) {
+    if ((gender !== "мужской" && gender !== "женский") || (users === [])) {
+      return 0;
+    } else {
+      return users.filter(user => user.gender === gender)
+          .map(user => user.age)
+          .reduce((usAge, next, index, genderArr) => {
+              usAge += next;
+              if (index === genderArr.length - 1) {
+                  return usAge / genderArr.length;
+              }
+              return usAge;
+          }, 0)
+    }
+  }
